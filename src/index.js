5ddb9fb54ba4319857bbe6270a9ed4cf60650dc0
陈辉 <9934042+chenhui134@user.noreply.gitee.com>
2022-09-06 19:37:21 +0800

    列表渲染

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import  {createRoot}  from "react-dom/client";
 
+let list=['🍉','🍍','🍌','🥭','🥝']
+
 let Ul=<>
-<li>4516</li>
-<li>4516</li>
-<li>4516</li>
-<li>4516</li>
-<li>4516</li>
+{list.map((item,i)=><li key={i}>{item}</li>)}
 </>
 let Div=React.createElement('div',{className:'aaaa'},Ul)
 
