13558556ed08c46a4c0c435c15723a197df2b41d
陈辉 <9934042+chenhui134@user.noreply.gitee.com>
2022-09-06 21:50:48 +0800

    Css样式的运用

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import  {createRoot}  from "react-dom/client";
-
+import './css/index.css'
 let list=['🍉','🍍','🍌','🥭','🥝']
 
+const Css={color:'#000'}
 let Ul=<>
-{list.map((item,i)=><li key={i}>{item}</li>)}
+{list.map((item,i)=><li style={{backgroundColor:'yellow',...Css}} key={i}>{i}{item}</li>)}
 </>
 let Div=React.createElement('div',{className:'aaaa'},Ul)
 
