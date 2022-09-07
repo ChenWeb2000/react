d4e8e3763c87c3b5788969d6d96f951d7f633172
陈辉 <9934042+chenhui134@user.noreply.gitee.com>
2022-09-07 18:58:04 +0800

    函数组件

---
+++ src/components/Sssub.js
@@ -0,0 +1,5 @@
+
+
+const Sssub=()=><span>子子子组件</span>
+
+export default Sssub
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from "react";
 import  {createRoot}  from "react-dom/client";
 import './css/index.css'
+
+import Sssub from './components/Sssub'
 let list=['🍉','🍍','🍌','🥭','🥝']
 
-const Css={color:'#000'}
-let Ul=<>
-{list.map((item,i)=><li style={{backgroundColor:'yellow',...Css}} key={i}>{i}{item}</li>)}
-</>
-let Div=React.createElement('div',{className:'aaaa'},Ul)
+let Ssub=()=>{
+    return list.map((item,i)=><li key={i}>{item}<Sssub/></li>)
+}
 
-createRoot(document.querySelector('#root')).render(Div)
+let Sub=()=><ul>
+    <Ssub />
+</ul>
+
+let Div=()=>{
+    return <div>
+        <h1>父组件</h1>
+        <Sub />
+    </div>
+}
+
+createRoot(document.querySelector('#root')).render(<Div />)
